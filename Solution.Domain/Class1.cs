2b645262be02172948375cc1869999b6a7529940
HsineGabsi <hsine.gabsi@esprit.tn>
2020-03-02 18:55:41 +0300

    test1

--- Solution.Domain/Class1.cs
+++ Solution.Domain/Class1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Solution.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,7 @@ namespace Solution.Domain
 {
     public class Class1
     {
+        git push --set-upstream origin hsine
+
     }
 }
